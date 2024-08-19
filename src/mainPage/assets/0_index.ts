4c329871b22c80de671ca1ccc358c68688e01ec4
sooy <ssy67366736@gmail.com>
2024-08-19 13:17:58 +0900

    fix: move repo err

--- src/mainPage/assets/0_index.ts
+++ src/mainPage/assets/0_index.ts
@@ -1,4 +1,4 @@
-import announceIMG from './announceImg.webp';
+import announceIMG from './announceIMG.webp';
 import IcDashboard from './dashboardIcon.svg?react';
 import IcTradeBots from './tradeBotsIcon.svg?react';
 import IcSearch from './searchIcon.svg?react';
